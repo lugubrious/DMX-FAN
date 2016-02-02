2cfbd487b1213d21849afd57b2d3b764d9666df4
Samuel Dewan <samdewan@me.com>
2016-02-02 16:14:35 -0500

    Created Pin_Definitions.h and initialization

---
+++ DMX-FAN/Pin_Definitions.h
@@ -0,0 +1,96 @@
+//
+//  Pin_Definitions.h
+//  DMX-FAN
+//
+//  Created by Samuel Dewan on 2016-02-02.
+//  Copyright © 2016 Samuel Dewan and Maxim Zakirov. All rights reserved.
+//
+
+#ifndef Pin_Definitions_h
+#define Pin_Definitions_h
+
+#include <avr/io.h>
+
+// MARK: LEDs
+#define RX_STATUS_PORT 	PORTD
+#define RX_STATUS_DDR	DDRD
+#define RX_STATUS_NUM  	PIND3
+#define RX_STATUS_PWM   OC2B
+
+// MARK: Motor Driving
+#define EN_1_2_PORT 	PORTD
+#define EN_1_2_DDR      DDRD
+#define EN_1_2_NUM  	PIND5
+
+#define EN_3_4_PORT 	PORTD
+#define EN_3_4_DDR      DDRD
+#define EN_3_4_NUM  	PIND6
+
+#define IN_1_PORT       PORTB
+#define IN_1_DDR        DDRB
+#define IN_1_NUM        PINB1 //OC1A
+#define IN_1_PWM        OC1A
+
+#define IN_2_PORT       PORTD
+#define IN_2_DDR        DDRD
+#define IN_2_NUM        PIND7
+
+#define IN_3_PORT       PORTB
+#define IN_3_DDR        DDRB
+#define IN_3_NUM        PINB2 //OC1B
+#define IN_3_PWM        OC1B
+
+#define IN_4_PORT       PORTB
+#define IN_4_DDR        DDRB
+#define IN_4_NUM        PINB0 //CLKO
+
+
+// MARK: DIP Switch
+
+#define DIP_0_PORT       PORTC
+#define DIP_0_DDR        DDRC
+#define DIP_0_PIN        PINC
+#define DIP_0_NUM        PINC5
+
+#define DIP_1_PORT       PORTC
+#define DIP_1_DDR        DDRC
+#define DIP_1_PIN        PINC
+#define DIP_1_NUM        PINC4
+
+#define DIP_2_PORT       PORTC
+#define DIP_2_DDR        DDRC
+#define DIP_2_PIN        PINC
+#define DIP_2_NUM        PINC3
+
+#define DIP_3_PORT       PORTC
+#define DIP_3_DDR        DDRC
+#define DIP_3_PIN        PINC
+#define DIP_3_NUM        PINC2
+
+#define DIP_4_PORT       PORTC
+#define DIP_4_DDR        DDRC
+#define DIP_4_PIN        PINC
+#define DIP_4_NUM        PINC1
+
+#define DIP_5_PORT       PORTC
+#define DIP_5_DDR        DDRC
+#define DIP_5_PIN        PINC
+#define DIP_5_NUM        PINC0
+
+#define DIP_6_PORT       PORTB
+#define DIP_6_DDR        DDRB
+#define DIP_6_PIN        PINB
+#define DIP_6_NUM        PINB5
+
+#define DIP_7_PORT       PORTB
+#define DIP_7_DDR        DDRB
+#define DIP_7_PIN        PINB
+#define DIP_7_NUM        PINB4
+
+#define DIP_8_PORT       PORTB
+#define DIP_8_DDR        DDRB
+#define DIP_8_PIN        PINB
+#define DIP_8_NUM        PINB3
+
+
+#endif /* Pin_Definitions_h */
--- DMX-FAN/main.c
+++ DMX-FAN/main.c
@@ -1,16 +1,119 @@
+//
+//  main.c
+//  DMX-FAN
+//
+//  Created by Samuel Dewan on 2016-02-02.
+//  Copyright © 2016 Samuel Dewan and Maxim Zakirov. All rights reserved.
+//
+
+
 #include <stdio.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+
+#include "Pin_Definitions.h"
+
+// MARK: Global Variables
+volatile uint32_t millis, lastRX;       //Clock values
+
+// MARK: Function Declarations
+static void main_loop (void);
+
+// MARK: Function Definitions
 
 void initIO(void) {
-	
+	// LEDs
+    RX_STATUS_DDR |= (1<<RX_STATUS_NUM);
+    // Motor
+    EN_1_2_DDR |= (1<<EN_1_2_NUM);
+    EN_3_4_DDR |= (1<<EN_3_4_NUM);
+    IN_1_DDR |= (1<<IN_1_NUM);
+    IN_2_DDR |= (1<<IN_2_NUM);
+    IN_3_DDR |= (1<<IN_3_NUM);
+    IN_4_DDR |= (1<<IN_4_NUM);
+    // DIP
+    DIP_0_DDR &= ~(1<<DIP_0_NUM);
+    DIP_0_PORT |= (1<<DIP_0_NUM);
+    DIP_1_DDR &= ~(1<<DIP_1_NUM);
+    DIP_1_PORT |= (1<<DIP_1_NUM);
+    DIP_2_DDR &= ~(1<<DIP_2_NUM);
+    DIP_2_PORT |= (1<<DIP_2_NUM);
+    DIP_3_DDR &= ~(1<<DIP_3_NUM);
+    DIP_3_PORT |= (1<<DIP_3_NUM);
+    DIP_4_DDR &= ~(1<<DIP_4_NUM);
+    DIP_4_PORT |= (1<<DIP_4_NUM);
+    DIP_5_DDR &= ~(1<<DIP_5_NUM);
+    DIP_5_PORT |= (1<<DIP_5_NUM);
+    DIP_6_DDR &= ~(1<<DIP_6_NUM);
+    DIP_6_PORT |= (1<<DIP_6_NUM);
+    DIP_7_DDR &= ~(1<<DIP_7_NUM);
+    DIP_7_PORT |= (1<<DIP_7_NUM);
+    DIP_8_DDR &= ~(1<<DIP_8_NUM);
+    DIP_8_PORT |= (1<<DIP_8_NUM);
 }
 
 int main(void) {
-	initIO();
+    cli();                                  // Clear interupts
+    
+	initIO();                               // Set pin modes
+    
+    // Timer 0 (Clock)
+    TCCR0A |= (1 << WGM01);                 // Set the Timer Mode to CTC
+    OCR0A = 125;                            // Set the value to count to 125 (1 millisecond at 8Mhz where prescaler = 64)
+    TIMSK0 |= (1 << OCIE0A);                // Set the ISR COMPA vector
+    TCCR0B |= (1 << CS01) | (1 << CS00);    // set prescaler to 64 (and therefor start timer 0)
+    
+    // Timer 1 (PWM 1)
+    TCCR1A |= 0xF0;							//Inverted mode
+    TCCR1A |= (1 << WGM10);					//Mode 5, Fast PWM, 8 bit
+    TCCR1B |= (1 << CS10) | (1 << WGM12);	//set prescaler to 0 and start timer 1
+    
+    // Timer 2 (PWM 2)
+    TCCR2A |= 0xF0;							//Inverted mode
+    TCCR2A |= (1 << WGM20)| (1 << WGM21);	//Mode 3, fast PWM
+    TCCR2B |= (1 << CS20);					//set prescaler to 0 and start timer 2
 
-	while (1) {
+    //Initialize Values to 0
+    IN_1_PWM = 0;
+    IN_3_PWM = 0;
+    RX_STATUS_PWM = 0;
+    
+    //Initialize USART
+    UBRR0H = 0;								//250kbps baud rate for a 8MHz system clock
+    UBRR0L = 1;
+    UCSR0A = 0;								//Only receiver enabled, RX interrupt enabled
+    UCSR0B = 0x90;
+    UCSR0C = 0x8e;							//8 data bits, 2 stop bits
+    
+    sei();                  // Enable interupts
+    
+    for (;;) {              // Run main loop for evermore
+        main_loop();
 	}
-	return 0; // never reached
+	return 0;               // never reached
 }
+
+void main_loop (void) {
+    
+}
+
+
+// MARK: Interupt Service Routines
+ISR (TIMER0_COMPA_vect) { 					//timer0 overflow interrupt (called every millisecond)
+    millis++; 								//Increment clock every millisecond
+}
+
+/*
+ DMX Channels
+ 
+ Chan 1: Fan one speed
+ Chan 2: Fan two speed
+ Chan 3: Direction control
+ 
+ F = Forward	R = Reverse
+ 
+ M1		M2			Value
+ F		F			0     - 64		(0% - 0.2509803922%)				Use 20%
+ F		R			65   - 128		(0.2549019608% - 0.5019607843%)		Use 40%
+ R		F			129 - 192		(0.5058823529% - 0.7529411765%)		Use 60%
+ R		R			193 - 255		(0.7568627451% - 1%)				Use 80%
+*/
